b0445dc5211eaf4263ac8d23cea44676845dd752
Blodir <pyrymakinencs@gmail.com>
2018-01-30 11:10:36 +0200

    Navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'vrs-root',
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,17 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApiService } from './api/api.service';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
-  declarations: [],
-  providers: []
+  declarations: [NavbarComponent],
+  providers: [],
+  exports: [ NavbarComponent, RouterModule ]
 })
 export class SharedModule {
   static forRoot() : ModuleWithProviders {
