5a301b1f54ab3377dfb2009b4fb82e397d2ec074
Radytin <Kreetta.hamalainen@helsinki.fi>
2018-01-30 12:34:54 +0200

    lisätty NGModuleen importti searchcomponentista

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,15 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApiService } from './api/api.service';
+import {SearchComponent} from './googlesearch/search/search.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [],
+  declarations: [
+    SearchComponent
+  ],
   providers: []
 })
 export class SharedModule {
