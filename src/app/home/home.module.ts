7f05dc478fb60ca1f52d872f270370b97556b361
Radytin <Kreetta.hamalainen@helsinki.fi>
2018-02-01 12:33:07 +0200

    Korjaus testien virheilmoituksiin

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
-
 import { routing } from './home.routing';
+import { SearchComponent } from '../shared/googlesearch/search/search.component';
+
 
 @NgModule({
   imports: [
     routing,
     CommonModule,
-    SharedModule
-
+    SharedModule,
+  
+    
   ],
+ 
   declarations: [HomeComponent]
 })
+
 export class HomeModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,5 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApiService } from './api/api.service';
 import { SearchComponent } from './googlesearch/search/search.component';
@@ -6,11 +7,13 @@ import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 
+
 @NgModule({
   imports: [
     CommonModule,
     RouterModule
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   declarations: [NavbarComponent, FooterComponent, SearchComponent],
   providers: [],
   exports: [ NavbarComponent, RouterModule, FooterComponent, SearchComponent ]
