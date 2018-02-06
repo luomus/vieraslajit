858bef35c2697ab4f26b61cf3a30e40cecd019a7
Arttu <arttukuronen@gmail.com>
2018-02-06 10:46:11 +0200

    Add env variable

--- src/app/shared/api/api.service.ts
+++ src/app/shared/api/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../../environments/environment.local';
+import { environment } from '../../../environments/environment';
 import { Taxonomy, TaxonomyDescription } from '../model/Taxonomy';
 import { Observable } from 'rxjs/Observable';
 import { PagedResult } from '../model/PagedResult';
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,9 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  lajiApi: {
+    url: 'https://api.laji.fi/v0/',
+    accessToken: 'pAec01FKjdmHNajib8n6QAbt9dDDpEVYYeFfEj6baYL697FcosZDyrAkLsbWyAH9'
+  }
 };
