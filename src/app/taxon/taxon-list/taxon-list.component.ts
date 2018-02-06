d838289c4b6bdbe3d5b587d6e112e1bb870595fe
Blodir <pyrymakinencs@gmail.com>
2018-02-06 10:01:07 +0200

    aot for taxon-list fixed

--- src/app/taxon/taxon-list/taxon-list.component.ts
+++ src/app/taxon/taxon-list/taxon-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'vrs-taxon-list',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./taxon-list.component.scss']
 })
 export class TaxonListComponent implements OnInit {
+
+  @Input() search = "";
 
   selected = [];
 
