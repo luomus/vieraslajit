5c819015f209c7a9477bacff306782a9250fb09a
Arttu <arttukuronen@gmail.com>
2018-02-18 14:05:34 +0200

    Fix pipe test

--- src/app/shared/pipe/label.pipe.spec.ts
+++ src/app/shared/pipe/label.pipe.spec.ts
@@ -1,8 +1,20 @@
 import { LabelPipe } from './label.pipe';
+import { MetadataService } from '../service/metadata.service';
+import { inject, TestBed } from '@angular/core/testing';
+import { ApiService } from '../api/api.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('LabelPipe', () => {
-  it('create an instance', () => {
-    const pipe = new LabelPipe();
-    expect(pipe).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [MetadataService, ApiService]
+    });
   });
+
+  it('create an instance', inject([MetadataService], (service: MetadataService) => {
+    const pipe = new LabelPipe(service);
+    expect(pipe).toBeTruthy();
+  })
+  );
 });
