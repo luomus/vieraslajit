e1f16f64feb4db9a609f1f51abf8e135154daacf
rohamo <rohamo@cs.helsinki.fi>
2018-02-21 02:20:39 +0200

    taxon list grid viewit, kuvat apista puuttuu ja muutama placeholderi vielä paikallaan

--- src/app/taxon/taxon-list/taxon-list.component.ts
+++ src/app/taxon/taxon-list/taxon-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
 import { PagedResult } from '../../shared/model/PagedResult';
-import { Taxonomy, TaxonomyDescription } from '../../shared/model/Taxonomy';
+import { Taxonomy, TaxonomyDescription, TaxonomyImage } from '../../shared/model/Taxonomy';
 import { TaxonService } from '../../shared/service/taxon.service';
 import { Informal } from '../../shared/model/Informal';
 import { TranslateService } from '@ngx-translate/core';
@@ -16,8 +16,10 @@ export class TaxonListComponent implements OnInit {
 
   @Input() search = '';
 
+  id: string;
   selected = [];
-
+  changeView: false;
+  media$: Array<TaxonomyImage>;
   taxa$: Taxonomy[];
   groups$: Informal[];
   selectedGroup: Informal;
@@ -28,8 +30,12 @@ export class TaxonListComponent implements OnInit {
     this.taxonService.getInformalGroups(this.translateService.currentLang).subscribe((data) => {
       this.groups$ = data.results;
     });
-  }
 
+    this.taxonService.getTaxonMedia(this.id, this.translateService.currentLang).subscribe(data => {
+      this.media$ = data;
+    });
+  }
+  
   onSearchChange(value) {
     let _selected = [];
     for (let t of this.taxa$) {
@@ -43,7 +49,7 @@ export class TaxonListComponent implements OnInit {
 
   onGroupSelect(target) {
     this.selectedGroup = target;
-    this.taxonService.getTaxonomy('MX.37600', this.translateService.currentLang, this.selectedGroup.id).subscribe(data => {
+    this.taxonService.getTaxonomy('MX.37600',  this.selectedGroup.id, this.translateService.currentLang).subscribe(data => {
       this.taxa$ = data.results;
       this.selected = this.taxa$;
     });
