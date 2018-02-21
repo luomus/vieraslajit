70b02e5c7594479faa7348155f48d8a8023250df
Arttu <arttukuronen@gmail.com>
2018-02-21 17:59:16 +0200

    Add translations to taxon card and home component

--- src/app/shared/service/taxon.service.ts
+++ src/app/shared/service/taxon.service.ts
@@ -32,7 +32,7 @@ export class TaxonService {
   // Get taxon description.
   getTaxonDescription(taxonId: string, lang: string): Observable<Array<any>> {
     return this.apiService
-      .taxonomyFindById(LajiApi.Endpoints.description, taxonId, { lang: lang, blacklist: 'eol:api' });
+      .taxonomyFindById(LajiApi.Endpoints.description, taxonId, { lang: lang, langFallback: false });
   }
   // Get taxon media. (Images).
   getTaxonMedia(taxonId: string, lang: string): Observable<Array<TaxonomyImage>> {
@@ -42,14 +42,15 @@ export class TaxonService {
   // Get taxon parents up to Biota
   getTaxonParents(taxonId: string, lang: string): Observable<Array<Taxonomy>> {
     return this.apiService
-      .taxonomyFindById(LajiApi.Endpoints.taxonParents, taxonId, { lang: lang, selectedFields: 'id, vernacularName, scientificName, taxonRank' });
+      .taxonomyFindById(LajiApi.Endpoints.taxonParents, taxonId,
+        { lang: lang, selectedFields: 'id, vernacularName, scientificName, taxonRank' });
   }
-  //Get autocomplete for taxon search.
+  // Get autocomplete for taxon search.
   getAutocomplete(field: string, q: string): Observable<any> {
     return this.apiService
       .autocompleteFindByField(LajiApi.Endpoints.autocomplete, field, { q, includePayload: true, onlyInvasive: true });
   }
-  //Get warehouse query count for taxon search.
+  // Get warehouse query count for taxon search.
   getWareHouseQueryCount(count: string, lang: string, taxonId): Observable<any> {
     return this.apiService
       .warehouseQueryCountGet(LajiApi.Endpoints.warehousequerycount, count, { cache: false, taxonId, individualCountMin: 1 });
