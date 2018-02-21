654631100a6f5c9f6148e8a7962f3777d722f122
Arttu <arttukuronen@gmail.com>
2018-02-21 12:17:28 +0200

    Fix pipe and card tests

--- src/app/shared/pipe/label.pipe.spec.ts
+++ src/app/shared/pipe/label.pipe.spec.ts
@@ -3,17 +3,19 @@ import { MetadataService } from '../service/metadata.service';
 import { inject, TestBed } from '@angular/core/testing';
 import { ApiService } from '../api/api.service';
 import { HttpClientModule } from '@angular/common/http';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 describe('LabelPipe', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule],
+      imports: [HttpClientModule,
+        TranslateModule.forRoot()],
       providers: [MetadataService, ApiService]
     });
   });
 
-  it('create an instance', inject([MetadataService], (service: MetadataService) => {
-    const pipe = new LabelPipe(service);
+  it('create an instance', inject([MetadataService, TranslateService], (service: MetadataService, translate: TranslateService) => {
+    const pipe = new LabelPipe(service, translate);
     expect(pipe).toBeTruthy();
   })
   );
