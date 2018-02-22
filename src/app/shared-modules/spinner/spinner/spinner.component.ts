933547978b391df324266f229481153983215a25
metinen <maria.ilvonen@gmail.com>
2018-02-22 13:48:15 +0200

    spinner shared module added

---
+++ src/app/shared-modules/spinner/spinner/spinner.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'laji-spinner',
+  template: `
+<div class="spinner three-bounce-spinner"
+  *ngIf="spinning"
+  [ngClass]="{'overlay-spinner': overlay, 'inline-spinner': !overlay, 'light': light}">
+  <div class="bounce1"></div>
+  <div class="bounce2"></div>
+  <div class="bounce3"></div>
+</div>
+<ng-content *ngIf="!hideContentWhileLoading || !spinning"></ng-content>`,
+  styleUrls: ['./spinner.component.css']
+})
+export class SpinnerComponent implements OnInit {
+
+  @Input() spinning = true;
+  @Input() overlay = false;
+  @Input() light = false;
+  @Input() hideContentWhileLoading = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
