c8f8bf55f1d561ffe96151e06e322ee271166157
metinen <maria.ilvonen@gmail.com>
2018-02-25 19:33:20 +0200

    newspage pagination previous and next work

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -12,11 +12,22 @@ import { PagedResult } from '../shared/model/PagedResult';
 })
 export class NewsComponent implements OnInit {
 
-  private data: PagedResult<NewsElement>;
+  //private data: PagedResult<NewsElement>;
   private news: Array<NewsElement> = [];
+  private newsView: Array<NewsElement>= [];
   private subTrans: Subscription;
-  private pages: Array<number> = [];
-  private pageSize = 5;
+  private pages: Array<number> = [1,2,3]
+  private pageSizeFetch = 50;
+  private pageSizeView=5;
+  private fetchedTimes: number=0;
+  /* MI: Koska API:sta ei voi hakea uutisia tagilla, on haettava yli halutun uutismäärän/sivu,
+  jotta saadaan uutissivulle haluttu määrä vieraslajiuutisia ilman toistuvia API-hakuja, kun osa haetuista
+  uutisista suodatetaan pois ja "sivu jää vajaaksi".
+  Tämä tarkoittaa käytännössä, että menetetään API:n ominaisuus tuoda uutiset valmiiksi sivutettuna eli 
+  voisi myös kysyä asiakkaalta, saisiko API:in suodatuksen tagilla (ja päivämäärällä)!
+  Mietin myös uutisdatan hakua kaikki kerralla, jolloin olisi voinut pitää ui:ssa tiedon sivujen määrästä.
+  Tällä hetkellä suomeksi "vain" n.220 uutisobjetkia, muttei ehkä se skaalautuvin ratkaisu
+  */
 
   constructor(private newsService: NewsService, private translate: TranslateService) { }
 
@@ -25,8 +36,8 @@ export class NewsComponent implements OnInit {
     this.getNews(1);
   }
 
-  getNews(page) {
-    this.newsService.getPage(page, this.pageSize.toString(), this.translate.currentLang).subscribe((data) => {
+  getNews(pageAPI) {
+    this.newsService.getPage(pageAPI, this.pageSizeFetch.toString(), this.translate.currentLang).subscribe((data) => {
 
      /* 
       laji.fi API:ista ei vielä tule "vieraslajit.fi" tagilla uutisia joissa contentia (vain externalUrl) 
@@ -35,14 +46,40 @@ export class NewsComponent implements OnInit {
       */
       this.news= data.results
         .filter(newsElement => newsElement.tag.includes(("technical")) || newsElement.tag.includes(("vieraslajit.fi")));
- 
+      
+      this.fetchedTimes++;
+      
+      if (this.fetchedTimes==1){
+      this.populateView(pageAPI);
+      }
+      /* 
       this.data = data;
-
       for(let i = 0; i < data.lastPage; i++) {
         this.pages.push(i+1); 
-      } 
+    }*/ 
 
     });
+  }
+
+  previous(){
+    let current = this.pages.length;
+    this.pages.pop();
+    this.populateView(current-1);
+  }
+
+  next(){
+    let nextPage= this.pages.length+1;
+    this.pages.push(nextPage);
+    this.populateView(nextPage);
+  }
+
+  populateView(pageView){
+    let newsNeeded= this.pageSizeView * pageView;
+
+    if (this.news.length < newsNeeded){
+      this.getNews(this.fetchedTimes+1);
+    }
+    this.newsView= this.news.slice(newsNeeded-this.pageSizeView,newsNeeded);  
   }
 
 }
