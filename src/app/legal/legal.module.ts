a192125a1e4b4a8b02454e76a78a72d54b3252d5
Radytin <Kreetta.hamalainen@helsinki.fi>
2018-02-26 09:19:09 +0200

    backup

--- src/app/legal/legal.module.ts
+++ src/app/legal/legal.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LegalComponent } from './legal.component';
+import { routing } from './legal.routing';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    routing,
+    SharedModule
   ],
   declarations: [LegalComponent]
 })
