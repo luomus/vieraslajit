6b955d08f7f6746230edb10efa36cdb3a2ec218b
metinen <maria.ilvonen@gmail.com>
2018-03-22 22:00:41 +0200

    newspage added method for feature image or fallback picture

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -39,8 +39,13 @@ export class NewsComponent implements OnInit {
     });
   }
 
-  /*setImageToDisplay(){
-    if (this.news.)
-  }*/
+  getImageToDisplay(newsElement: NewsElement){
+    if(newsElement.hasOwnProperty("featuredImage")){
+      this.imageToDisplay=newsElement.featuredImage;
+    } else {
+      this.imageToDisplay= "https://media.istockphoto.com/photos/sunrise-on-yosemite-valley-picture-id505872990?k=6&m=505872990&s=612x612&w=0&h=XcdHhkC9PF9-saYT6n_GQD-0Hf8dbI_Q4wfYlZZGpNk=";
+    } 
+    return this.imageToDisplay;
+  }
 
 }
