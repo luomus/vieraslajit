3a39404f7adf7966cf07b1eba38cde949d9332c6
Arttu <arttukuronen@gmail.com>
2018-04-02 15:03:46 +0300

    Add service for sending documents

---
+++ src/app/shared/service/document.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { ApiService, LajiApi } from '../api/api.service';
+import { Document } from '../model/Document';
+
+@Injectable()
+export class DocumentService {
+
+  constructor(private apiService: ApiService) { }
+
+
+  createDocument(userToken: string, data: Document): Observable<Document> {
+    return this.apiService.documentApi(LajiApi.Endpoints.createDocument, userToken, data);
+  }
+}
