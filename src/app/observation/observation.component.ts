23ff094bb94e06cb27072e3da62f8d1c18de549a
kivipaperisakset <kivipaperisakset@users.noreply.github.com>
2018-04-03 18:56:11 +0300

    working observation map put into taxon-card

--- src/app/observation/observation.component.ts
+++ src/app/observation/observation.component.ts
@@ -3,21 +3,24 @@ import { ObservationService } from '../shared/service/observation.service';
 import { WarehouseQueryList } from '../shared/model/Warehouse';
 import { PagedResult } from '../shared/model/PagedResult';
 import { Subscription } from 'rxjs/Subscription';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'vrs-observation',
   templateUrl: './observation.component.html',
   styleUrls: ['./observation.component.scss']
 })
-export class ObservationComponent implements OnInit {
 
+export class ObservationComponent implements OnInit {
   @Input() id: string;
   private idArray: Array<string> = [];
   observations: Array<WarehouseQueryList> = [];
   private pageSize: string;
   private mapObservations;
 
-  constructor(private observationService: ObservationService) { }
+  constructor(
+    private observationService: ObservationService,
+    public translate: TranslateService) { }
 
   ngOnInit() {
     this.idArray.push(this.id);
@@ -26,20 +29,30 @@ export class ObservationComponent implements OnInit {
     this.initializeMap();
   }
 
-  initializeMap(){
+  initializeMap() {
     const options = {
-      rootElem: document.getElementById("map")
-    }  
+      rootElem: document.getElementById("map"),
+      lang: this.translate.currentLang,
+      popupOnHover: false,
+      center: {
+        "lat": 65.5,
+        "lng": 27
+      },
+      zoom: 1,
+      tileLayerName: "openStreetMap", 
+      controls: {
+        coordinates: true,
+    	},
+    };
+
     var LajiMap = require("laji-map").default;
-    this.mapObservations = new LajiMap(options);  
+    this.mapObservations = new LajiMap(options);
   }
 
   update() {
     this.observationService.getObservationsById(this.idArray, this.pageSize).subscribe(data => {
       this.observations= data.results;
-    });  
+    });
   }
-
-
 
 }
