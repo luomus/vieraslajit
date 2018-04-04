c5cf4f214a141e20751f6e59f290daa42a4b8a1c
metinen <maria.ilvonen@gmail.com>
2018-04-04 20:28:31 +0300

    observations to map @ ERROR Error: Invalid LatLng object: (undefined, undefined)

--- src/app/observation/observation.component.ts
+++ src/app/observation/observation.component.ts
@@ -13,10 +13,9 @@ import { TranslateService } from '@ngx-translate/core';
 
 export class ObservationComponent implements OnInit {
   @Input() id: string;
-  private idArray: Array<string> = [];
-  observations: Array<WarehouseQueryList> = [];
-  private pageSize: string;
-  private mapObservations;
+  private idArray: Array<string>=[];
+  private pageSize: string = "1000";
+  map;
 
   constructor(
     private observationService: ObservationService,
@@ -24,35 +23,89 @@ export class ObservationComponent implements OnInit {
 
   ngOnInit() {
     this.idArray.push(this.id);
-    this.pageSize= "10000";
-    this.update();
     this.initializeMap();
   }
 
-  initializeMap() {
+  initializeMap() {       
+    var LajiMap = require("laji-map").default;
+    this.map = new LajiMap(this.mapOptions());
+  }
+
+  mapOptions(){
     const options = {
       rootElem: document.getElementById("map"),
       lang: this.translate.currentLang,
       popupOnHover: false,
-      center: {
+      /*center: {
         "lat": 65.5,
         "lng": 27
-      },
+      },*/
       zoom: 1,
+      zoomToData : true,
       tileLayerName: "openStreetMap", 
-      controls: {
-        coordinates: true,
-    	},
+      controls: {  
+      },
+      data: this.getMapData()
     };
-
-    var LajiMap = require("laji-map").default;
-    this.mapObservations = new LajiMap(options);
+    return options;
   }
 
-  update() {
+  getMapData() {
+    const observations=this.queryObservations();
+    let coordinates = [];
+
+    if (observations){
+      
+      //let coordinates = [];      
+      observations.map(observation => {
+        console.log(observation.gathering.conversions.wgs84CenterPoint.lon);
+        //coordinates.push(observation.gathering.conversions.wgs84CenterPoint.lon);
+        //coordinates.push(observation.gathering.conversions.wgs84CenterPoint.lat);
+        coordinates = [
+          observation.gathering.conversions.wgs84CenterPoint.lon,
+          observation.gathering.conversions.wgs84CenterPoint.lat
+        ]
+        
+      })
+      console.log(coordinates);
+      const dataObject= this.featuresToFeatureCollection(this.coordinatesToFeatures(coordinates));
+      //console.log(dataObject);
+      let dataArray = [];
+      dataArray.push(dataObject);
+      return dataArray;  
+    }
+  }
+
+  coordinatesToFeatures (coordinates){
+    const features = [];
+    features.push(
+      {
+        'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': coordinates
+        }
+    })
+    return features;
+  }
+  
+  featuresToFeatureCollection(features){
+    const dataObject= {
+    featureCollection: {
+      'type': 'FeatureCollection',
+      'features': features
+    }
+    }
+    return dataObject;
+  }
+
+  queryObservations() {
+    let observations=[];
     this.observationService.getObservationsById(this.idArray, this.pageSize).subscribe(data => {
-      this.observations= data.results;
+      observations= data.results;
     });
-  }
-
+    return observations;
+  }   
+  
 }
+ 
