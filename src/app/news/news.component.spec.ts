ab09b9f700dd1affe2a1b0f0452e76613cf97379
metinen <maria.ilvonen@gmail.com>
2018-04-10 14:58:20 +0300

    news styling fixed duplicate author&date text and missing imports in test class

--- src/app/news/news.component.spec.ts
+++ src/app/news/news.component.spec.ts
@@ -5,6 +5,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { NewsService } from '../shared/service/news.service';
 import { NewsComponent } from './news.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { SharedModule } from '../shared/shared.module';
+import { EditcmsComponent } from '../shared-modules/editcms/editcms/editcms.component';
 
 describe('NewsComponent', () => {
   let component: NewsComponent;
@@ -12,8 +14,8 @@ describe('NewsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NewsComponent ],
-      imports: [HttpClientModule, TranslateModule.forRoot()],
+      declarations: [ NewsComponent, EditcmsComponent ],
+      imports: [HttpClientModule, TranslateModule.forRoot(),SharedModule],
       providers: [ApiService, NewsService]
     })
     .compileComponents();
