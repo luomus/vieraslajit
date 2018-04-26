ec913e1cc3531c4bf24b13e5521624c6c866d063
Arttu <arttukuronen@gmail.com>
2018-04-26 13:26:07 +0300

    Add english and swedish content

--- src/assets/i18n/cms-content.ts
+++ src/assets/i18n/cms-content.ts
@@ -4,22 +4,26 @@ export enum StaticContent {
 
 export function findContentID(target: StaticContent, lang: string) {
     let r: string = "";
-    switch(target) {
+    switch (target) {
         case StaticContent.EUList:
-            if (lang== "fi") r = "i-113";
-            if (lang== "en") r = "i-117";
-            if (lang== "sv") r = "i-121";
+            if (lang == "fi") r = "i-113";
+            if (lang == "en") r = "i-117";
+            if (lang == "sv") r = "i-121";
             break;
         case StaticContent.FIList:
-            if (lang== "fi") r = "i-115";
-            if (lang== "en") r = "i-119";
-            if (lang== "sv") r = "i-124";
+            if (lang == "fi") r = "i-115";
+            if (lang == "en") r = "i-119";
+            if (lang == "sv") r = "i-124";
             break;
         case StaticContent.EradicationInfo:
-            if(lang=="fi") r = "i-237";
+            if (lang == "fi") r = "i-237";
+            if (lang == "en") r = "i-244";
+            if (lang == "sv") r = "i-246";
             break;
         case StaticContent.EradicationGuides:
-            if(lang=="fi") r = "i-239"
+            if (lang == "fi") r = "i-239";
+            if (lang == "en") r = "i-255";
+            if (lang == "sv") r = "i-252";
             break;
     }
     return r;
