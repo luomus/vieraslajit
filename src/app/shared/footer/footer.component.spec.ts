b459fd6660f36974ab8c53441da75867e5c2d6fc
Blodir <Amwrailth@gmail.com>
2018-05-03 20:41:56 +0300

    fix footer links and integrate with cms

--- src/app/shared/footer/footer.component.spec.ts
+++ src/app/shared/footer/footer.component.spec.ts
@@ -2,6 +2,11 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
 import { DebugElement } from '@angular/core/src/debug/debug_node';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { InformationService } from '../service/information.service';
+import { ApiService } from '../api/api.service';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -10,7 +15,9 @@ describe('FooterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
+      imports: [ TranslateModule.forRoot(), RouterTestingModule, HttpClientModule ],
+      declarations: [ FooterComponent ],
+      providers: [TranslateService, InformationService, ApiService]
     })
     .compileComponents();
   }));
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService, Role } from './../service/user.service';
 import { InformationService } from '../service/information.service';
+import { findContentID, StaticContent } from '../../../assets/i18n/cms-content';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'vrs-footer',
@@ -11,11 +13,20 @@ export class FooterComponent implements OnInit {
 
   sPages: Array<any> = new Array();
 
-  constructor(private informationService: InformationService) { }
+  constructor(private informationService: InformationService, private translate: TranslateService) { }
 
   ngOnInit() {
-    this.informationService.getInformation("i-2").subscribe((data) => {
-
+    this.informationService.getInformation(findContentID(StaticContent.Root, this.translate.currentLang)).subscribe((data) => {
+      for(let c of data.children) {
+        this.sPages.push(c);
+        this.informationService.getInformation(c.id).subscribe((data2) => {
+          if(data2.children) {
+            for(let c2 of data2.children) {
+              this.sPages.push(c2);
+            }
+          }
+        });
+      }
     });
   }
 
