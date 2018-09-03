361635f0e8400e590f977fca5c65d808f6aae753
Mäkinen Pyry V <pyry.makinen@helsinki.fi>
2018-09-03 13:49:26 +0300

    observatoins fix rxjs import

--- src/app/observations/observations.component.ts
+++ src/app/observations/observations.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { WarehouseQueryList } from '../shared/model/Warehouse';
 import { PagedResult } from '../shared/model/PagedResult';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'vrs-observations',
