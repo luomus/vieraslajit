b3e98674258f53927de8016ce90d5a47e408d15c
Mäkinen Pyry V <pyry.makinen@helsinki.fi>
2019-02-01 13:06:42 +0200

    fix google search service in prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,5 +7,10 @@ export const environment = {
   lajiAuth: {
     authUrl: 'https://fmnh-ws-test.it.helsinki.fi/laji-auth/',
     systemID: 'KE.781'
+  },
+  googleSearch: {
+    url: 'https://www.googleapis.com/customsearch/v1',
+    key: 'AIzaSyCReh-YJ3pDZv5ngzGQaZoPANqi8nYDYQs',
+    engineId: '006132177942388329814:wyntd_p9woi'
   }
 };
