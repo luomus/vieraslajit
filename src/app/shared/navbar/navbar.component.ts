4d2d700a5b1b10ccc02e5e4203b14b3e03a6882e
Mäkinen Pyry V <pyry.makinen@helsinki.fi>
2019-02-20 16:32:36 +0200

    styling, shorter navbar, change hex values to hsl

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -69,7 +69,7 @@ export class NavbarComponent implements OnInit, AfterViewChecked, AfterViewInit,
 
   private updateFixedTop() {
     const _fixedTop = this.fixedTop;
-    if ($(window).scrollTop() <= 108) {
+    if ($(window).scrollTop() < 75) {
         this.fixedTop = false;
     } else {
         this.fixedTop = true;
