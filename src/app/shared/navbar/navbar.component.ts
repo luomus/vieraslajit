b53fd911c3a74d3fa17d51c4afdc0bd629b930bd
Blodir <Amwrailth@gmail.com>
2019-02-21 22:08:27 +0200

    style

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -69,7 +69,7 @@ export class NavbarComponent implements OnInit, AfterViewChecked, AfterViewInit,
 
   private updateFixedTop() {
     const _fixedTop = this.fixedTop;
-    if ($(window).scrollTop() < 75) {
+    if ($(window).scrollTop() < 86) {
         this.fixedTop = false;
     } else {
         this.fixedTop = true;
