a546f132037579a38772944bd34272a4c97c5449
Mäkinen Pyry V <pyry.makinen@helsinki.fi>
2019-03-04 09:32:07 +0200

    enable scroll position restoration

--- src/app/vrs-router.module.ts
+++ src/app/vrs-router.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })
+    RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload', scrollPositionRestoration: 'enabled' })
   ],
   exports: [
     RouterModule
