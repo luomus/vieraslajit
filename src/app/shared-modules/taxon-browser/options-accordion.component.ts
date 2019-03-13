c73c60a56d80ae1585661a3bd1797f8dbf8d9dfc
Mäkinen Pyry V <pyry.makinen@helsinki.fi>
2019-03-13 15:39:42 +0200

    taoxn browser options fix tooltip bug

--- src/app/shared-modules/taxon-browser/options-accordion.component.ts
+++ src/app/shared-modules/taxon-browser/options-accordion.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from "@angular/core";
 @Component({
     selector: 'options-accordion',
     template: `
-<div class="options-title" (click)="onClick($event)">{{title}}<div class="options-title-icon-container"><span class="oi" [ngClass]="{'oi-chevron-right': !active, 'oi-chevron-bottom': active}"></span></div></div>
+<div class="options-header" (click)="onClick($event)">{{header}}<div class="options-header-icon-container"><span class="oi" [ngClass]="{'oi-chevron-right': !active, 'oi-chevron-bottom': active}"></span></div></div>
 <div class="options-content" [ngClass]="{'hide': !active}">
     <ng-content></ng-content>
 </div>
@@ -11,7 +11,7 @@ import { Component, Input } from "@angular/core";
     styleUrls: [`options-accordion.component.scss`]
 })
 export class OptionsAccordionComponent {
-    @Input() title;
+    @Input() header;
     active = true;
     constructor(){}
     onClick(e) {
