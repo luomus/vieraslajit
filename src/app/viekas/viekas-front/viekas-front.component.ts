2991becff9943b47f9d1ba545e9fc1d28d33745d
Mäkinen Pyry V <pyry.makinen@helsinki.fi>
2019-05-21 15:16:08 +0300

    remove unused files

--- src/app/viekas/viekas-front/viekas-front.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    templateUrl: './viekas-front.component.html',
-    styleUrls: ['./viekas-front.component.scss']
-})
-export class ViekasFrontComponent {
-
-}
--- src/app/viekas/viekas.module.ts
+++ src/app/viekas/viekas.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ViekasComponent } from './viekas.component';
 import { ViekasRoutingModule } from './viekas-routing.module';
-import { ViekasFrontComponent } from './viekas-front/viekas-front.component';
 import { SharedModule } from '../shared/shared.module';
-import { ViekasStaticComponent } from './viekas-static/viekas-static.component';
 import { StaticModule } from 'app/static/static.module';
 
 @NgModule({
@@ -15,9 +12,6 @@ import { StaticModule } from 'app/static/static.module';
     StaticModule
   ],
   declarations: [
-    ViekasComponent,
-    ViekasFrontComponent,
-    ViekasStaticComponent
   ]
 })
 export class ViekasModule { }
