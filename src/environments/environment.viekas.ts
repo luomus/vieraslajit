a73bad2afc80702b5186d6d1e9755e6d888dd87b
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2019-05-27 22:23:31 +0300

    viekas production environment

---
+++ src/environments/environment.viekas.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: true,
+  vierasCollection: 'HR.2049',
+  baseUrl: 'viekas.laji.fi',
+  lajiApi: {
+    url: 'https://viekas.laji.fi/api'
+  },
+  lajiAuth: {
+    authUrl: 'https://login.laji.fi/laji-auth/',
+    systemID: 'KE.781'
+  },
+  googleSearch: {
+    url: 'https://www.googleapis.com/customsearch/v1',
+    key: 'AIzaSyCReh-YJ3pDZv5ngzGQaZoPANqi8nYDYQs',
+    engineId: '006132177942388329814:wyntd_p9woi'
+  }
+};
