fdec065f322f9e3b5af75a5c30eaf97a5eb960a3
Mäkinen Pyry V <pyry.makinen@helsinki.fi>
2019-08-16 12:49:24 +0300

    footer show image credit only when on homepage

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'vrs-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  onHomePage$: Observable<boolean>;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
+  ngOnInit() {
+    this.onHomePage$ = this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      map((event: NavigationEnd) => {
+        return event.url.length <= 1
+      })
+    )
+  }
 }
