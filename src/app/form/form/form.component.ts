bc265311f7067f173b3f067fc9e1d87158bdf13e
Mäkinen Pyry V <pyry.makinen@helsinki.fi>
2019-11-08 14:42:38 +0200

    destroy laji form

--- src/app/form/form/form.component.ts
+++ src/app/form/form/form.component.ts
@@ -140,5 +140,6 @@ export class FormComponent implements AfterViewInit, OnDestroy, OnInit {
     ngOnDestroy() {
         this.unsubscribe$.next();
         this.unsubscribe$.complete();
+        this.lajiFormWrapper.destroy();
     }
 }
