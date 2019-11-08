f3446a23a3be05929cbf930a9511e8efd1b6c31f
Mäkinen Pyry V <pyry.makinen@helsinki.fi>
2019-11-08 12:34:19 +0200

    improve html truncation

--- src/app/shared/pipe/truncate.pipe.ts
+++ src/app/shared/pipe/truncate.pipe.ts
@@ -6,10 +6,32 @@ import { Pipe, PipeTransform } from "@angular/core";
 export class TruncatePipe implements PipeTransform {
     constructor() {}
     transform(value: string, length: number): string {
-        let truncated = value.slice(0, length);
-        if (value.length > truncated.length) {
-            truncated = truncated.concat('...');
+        return this.truncateHtml(value, length);
+    }
+
+    truncateHtml(html: string, length: number): string {
+        let tagInnerOpen = false;
+        let outputString = '';
+        let count = 0;
+        for (let i = 0; i < html.length; i++) {
+            let char = html.charAt(i)
+            if (char === '<') {
+                tagInnerOpen = true;
+                outputString += char;
+            } else if (char === '>') {
+                tagInnerOpen = false;
+                outputString += char;
+            } else if (tagInnerOpen === true) {
+                outputString += char;
+            } else {
+                if (count < length) {
+                    outputString += char;
+                } else if (count === length - 1) {
+                    outputString += '...';
+                }
+                count++;
+            }
         }
-        return truncated;
+        return outputString;
     }
 }
