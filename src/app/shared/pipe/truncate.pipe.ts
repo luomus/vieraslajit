51ad814485ed298c7153a0b5874e328d309d1333
Mäkinen Pyry V <pyry.makinen@helsinki.fi>
2019-12-05 17:09:29 +0200

    improve html truncation

--- src/app/shared/pipe/truncate.pipe.ts
+++ src/app/shared/pipe/truncate.pipe.ts
@@ -6,33 +6,73 @@ import { Pipe, PipeTransform } from "@angular/core";
 export class TruncatePipe implements PipeTransform {
     constructor() {}
     transform(value: string, length: number): string {
-        return this.truncateHtml(value, length);
+        return this.truncateHTML(value, length);
     }
 
-    truncateHtml(html: string, length: number): string {
-        let tagInnerOpen = false;
-        let outputString = '';
-        let count = 0;
-        for (let i = 0; i < html.length; i++) {
-            let char = html.charAt(i)
-            if (char === '<') {
-                tagInnerOpen = true;
-                outputString += char;
-            } else if (char === '>') {
-                tagInnerOpen = false;
-                outputString += char;
-            } else if (tagInnerOpen === true) {
-                outputString += char;
-            } else {
-                if (count < length) {
-                    outputString += char;
-                }
-                if (count === length - 1) {
-                    outputString += '...';
-                }
-                count++;
+    /**
+     * 
+     * @param html 
+     * @param maxLength Maximum length of content (text inside tags). Tags, attributes etc. are unaffected.
+     */
+    truncateHTML(html: string, maxLength: number) {
+        const trimmedHtml = html.replace(/(\r\n|\n|\r|\t)/gm, "");
+
+        let outString = '';
+        let contentLength = 0;
+
+        let tagStack = [];
+        let tagString = '';
+        let writeTagString = false;
+        let innerTagOpen = false;
+        let closingTag = false;
+        for (let i = 0; i < trimmedHtml.length; i++) {
+            const char = trimmedHtml.charAt(i);
+            switch(char) {
+                case '<':
+                    innerTagOpen = true;
+                    tagString = '';
+                    writeTagString = true;
+                    closingTag = false;
+                    break;
+                case '>':
+                    innerTagOpen = false;
+                    writeTagString = false;
+                    if (contentLength > maxLength) {
+                        break;
+                    }
+                    if (closingTag) {
+                        // previous tag was closed
+                        tagStack.pop();
+                    } else {
+                        // an open tag was added
+                        tagStack.push(tagString)
+                    }
+                    break;
+                case '/':
+                    closingTag = true;
+                    break;
+                case ' ':
+                    writeTagString = false;
+                    break;
+                default:
+                    if (writeTagString) {
+                        tagString += char;
+                    }
+                    if (!innerTagOpen) {
+                        contentLength++;
+                    }
+                    break;
+            }
+            if (contentLength <= maxLength) {
+                outString += char;
+            }
+            if (contentLength === maxLength) {
+                outString += '...';
+                contentLength++;
             }
         }
-        return outputString;
+        // close all leftover tags in the stack
+        tagStack.reverse().forEach(tag => outString += `</${tag}>`);
+        return outString;
     }
 }
