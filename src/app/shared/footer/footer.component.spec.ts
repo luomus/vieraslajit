a4c875bb995de09ecd1f66c93cb9c0ad546e6a5d
Blodir <Amwrailth@gmail.com>
2020-02-05 11:02:51 +0200

    fix import in tests

--- src/app/shared/footer/footer.component.spec.ts
+++ src/app/shared/footer/footer.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
-import { DebugElement } from '@angular/core/src/debug/debug_node';
+import { DebugElement } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
