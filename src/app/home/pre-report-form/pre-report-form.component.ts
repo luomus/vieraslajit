84efe7f9a0aa355533dc7f07f998be193fa7d812
Mäkinen Pyry V <pyry.makinen@helsinki.fi>
2020-02-13 14:50:43 +0200

    home pre-report-form changes

--- src/app/home/pre-report-form/pre-report-form.component.ts
+++ src/app/home/pre-report-form/pre-report-form.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { TaxonService } from 'app/shared/service/taxon.service';
+import { TranslateService } from '@ngx-translate/core';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'vrs-pre-report-form',
   templateUrl: './pre-report-form.component.html',
   styleUrls: ['./pre-report-form.component.scss']
 })
-export class PreReportFormComponent implements OnInit {
+export class PreReportFormComponent implements OnInit, OnDestroy {
+  unsubscribe$ = new Subject<void>();
 
-  constructor() { }
+  linkEnabled = false;
+  customReportFormLink: string
+
+  constructor(private taxonService: TaxonService, private translate: TranslateService) { }
 
   ngOnInit() {
   }
 
+  onTaxonChange(id: string) {
+    this.linkEnabled = false;
+    if (id) {
+      this.taxonService.getTaxon(id, this.translate.currentLang, {
+        selectedFields: [
+          'customReportFormLink',
+        ],
+      }).pipe(takeUntil(this.unsubscribe$)).subscribe((res) => {
+        this.customReportFormLink = res.customReportFormLink
+        this.linkEnabled = true
+      })
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 }
