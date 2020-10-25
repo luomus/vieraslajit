f11c805093ee14363c4c71811e32d2d972a34081
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2020-10-25 19:28:31 +0200

    update document lang on langChange

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 import { UserService } from './shared/service/user.service';
 import { Router, NavigationEnd, NavigationStart } from '@angular/router';
 import { StateService } from './state.service';
 import { SwUpdate } from '@angular/service-worker';
 import { LoaderService } from './shared/service/loader.service';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 
 /**
  * Main component that acts as a container for navigation, content and footer.
@@ -27,8 +28,16 @@ export class AppComponent implements OnInit {
   * 1. Use English if a particular translation element is not found
   * 2. Use either the default language or language stored in localStorage
   */
-  constructor(state: StateService, translate: TranslateService, private userService: UserService,
-              private router: Router, private swUpdate: SwUpdate, private loaderService: LoaderService) {
+  constructor(
+    state: StateService,
+    translate: TranslateService,
+    private userService: UserService,
+    private router: Router,
+    private swUpdate: SwUpdate,
+    private loaderService: LoaderService,
+    @Inject(DOCUMENT) private document: Document,
+    @Inject(PLATFORM_ID) private platformId: object
+  ) {
 
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationStart) {
@@ -47,6 +56,12 @@ export class AppComponent implements OnInit {
     * Use English if translation is not found
     */
     translate.setDefaultLang('en');
+
+    this.translate.onLangChange.subscribe((e: LangChangeEvent) => {
+      if (isPlatformBrowser(this.platformId)) {
+        this.document.documentElement.lang = e.lang;
+      }
+    })
 
     /**
     * User's language choice is stored in localStorage, if language is not found
