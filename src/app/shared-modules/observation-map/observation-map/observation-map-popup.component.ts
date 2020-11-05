43e12ad4a4a493b38d59d4f90a1ad4cddac27c02
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2020-11-05 22:42:07 +0200

    improve observation map popup

--- src/app/shared-modules/observation-map/observation-map/observation-map-popup.component.ts
+++ src/app/shared-modules/observation-map/observation-map/observation-map-popup.component.ts
@@ -4,48 +4,18 @@ import { ObservationModalComponent } from "./observation-modal.component";
 
 @Component({
     template: `
-<table class="popup">
-    <tr class="header-row">
-        <td class="icon-container">
-            <a (click)="openModal(observationId, unitId)" class="oi oi-eye"></a>
-        </td>
-        <td>
-            <h5 translate>observation-map.popup.title</h5>
-        </td>
-    </tr>
-    <tr>
-        <td class="icon-container">
-            <a [routerLink]="['./lajit/', taxonId]" class="oi oi-info"></a>
-        </td>
-        <td>
-            {{name.charAt(0).toUpperCase() + name.substr(1)}}
-        </td>
-    </tr>
-    <tr>
-        <td></td>
-        <td>
-            {{municipality}}
-        </td>
-    </tr>
-    <tr>
-        <td></td>
-        <td>
-            {{date.substring(8, 10) + "." + date.substring(5, 7) + "." + date.substring(0, 4)}}
-        </td>
-    </tr>
-    <tr>
-        <td></td>
-        <td>
-            {{reliability}}
-        </td>
-    </tr>
-    <tr>
-        <td></td>
-        <td>
-            {{notes}}
-        </td>
-    </tr>
-</table>
+<div class="popup">
+<h5 translate>observation-map.popup.title</h5>
+<hr>
+<div>{{name.charAt(0).toUpperCase() + name.substr(1)}}</div>
+<div>{{municipality}}</div>
+<div>{{date.substring(8, 10) + "." + date.substring(5, 7) + "." + date.substring(0, 4)}}</div>
+<div>{{reliability}}</div>
+<div>{{notes}}</div>
+<hr>
+<a (click)="openModal(observationId, unitId)" class="popup-link"><span class="oi oi-eye"></span>Tarkastele havaintoa</a>
+<a [routerLink]="['./lajit/', taxonId]" class="popup-link"><span class="oi oi-info"></span>Lue lisää lajista</a>
+</div>
 `
 })
 export class ObservationMapPopupComponent {
