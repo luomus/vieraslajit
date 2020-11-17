16faf01e08a87d45426e173c757ad43059ecd130
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2020-11-17 21:27:36 +0200

    time selector add reset button

--- src/app/shared-modules/observation-map/observation-map/time-selector/time-selector.component.ts
+++ src/app/shared-modules/observation-map/observation-map/time-selector/time-selector.component.ts
@@ -3,7 +3,13 @@ import { Component, Output, EventEmitter, Renderer2, ViewChild, ElementRef, Inpu
 @Component({
     selector: 'vrs-time-selector',
     template: `
-    <input #timeInput [owlDateTime]="dt1" [owlDateTimeTrigger]="dt1" [selectMode]="'range'" (dateTimeChange)="onTimeChange($event)">
+    <div class="input-group mb-3">
+        <input class="form-control" #timeInput [owlDateTime]="dt1" [owlDateTimeTrigger]="dt1" [selectMode]="'range'" (dateTimeChange)="onTimeChange($event)">
+        <div class="input-group-append">
+            <button class="btn btn-outline-secondary remove" (click)="onRemove()" type="button">x</button>
+        </div>
+    </div>
+    
     <owl-date-time #dt1 [pickerType]="'calendar'" [firstDayOfWeek]="1"></owl-date-time>
     `, styleUrls: ['./time-selector.component.scss']
 })
@@ -20,5 +26,11 @@ export class TimeSelectorComponent {
 
     onTimeChange(event) {
         this.timeChangeEvent.emit(event.value);
+    }
+
+    onRemove() {
+        this.timeChangeEvent.emit(undefined);
+        this.timeInput.nativeElement.value = '';
+        this.setTimeValue('');
     }
 }
