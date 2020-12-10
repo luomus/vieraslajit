b9161ad80ba4bcdf8f4dbc3a1b5afdc7c98368fb
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2020-12-10 11:46:41 +0200

    add maplegend to mock module

--- src/app/shared-modules/observation-map-mock/observation-map.module.ts
+++ src/app/shared-modules/observation-map-mock/observation-map.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ObservationMapComponent } from './observation-map/observation-map.component';
+import { MapLegendComponent } from '@observation-map/observation-map/legend/map-legend.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    ObservationMapComponent
+    ObservationMapComponent,
+    MapLegendComponent
   ],
   exports: [
-    ObservationMapComponent
+    ObservationMapComponent,
+    MapLegendComponent
   ]
 })
 export class ObservationMapModule { }
