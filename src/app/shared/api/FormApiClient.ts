49be49a8f9971feb6b61223471766d9cb1268441
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2023-09-19 14:52:19 +0300

    laji-form fixes

--- src/app/shared/api/FormApiClient.ts
+++ src/app/shared/api/FormApiClient.ts
@@ -3,50 +3,39 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { TranslateService } from '@ngx-translate/core';
 import { UserService } from '../service/user.service';
-
+import { catchError, map, tap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable()
 export class FormApiClient {
   protected basePath = environment.lajiApi.url;
-  public defaultHeaders: HttpHeaders = new HttpHeaders();
 
   constructor(protected http: HttpClient, private translate: TranslateService) {}
 
-  public fetch(resource: string, query: any, options?: any): Promise<any> {
+  public fetch<T>(resource: string, query: any, options = {}): Promise<T> {
     const path = this.basePath + resource;
-
-    const queryParameters = new HttpParams();
-
-    queryParameters.set('lang', this.translate.currentLang);
-    queryParameters.set('personToken', UserService.getToken());
-
-    for (const param in query) {
-      if (!query.hasOwnProperty(param)) {
-        continue;
+    const queryParameters = new HttpParams({
+      fromObject: {
+        ...query,
+        lang: this.translate.currentLang,
+        personToken: UserService.getToken(),
+        excludeNameTypes: resource === '/autocomplete/taxon' ? 'MX.misappliedCircumscription,MX.misspelledCircumscription,MX.uncertainCircumscription' : undefined
       }
-      if (query[param] !== undefined) {
-        queryParameters.set(param, query[param]);
-      }
-    }
-    if (!options) {
-      options = {};
-    }
+    });
 
+    const method = options['method'] || 'GET';
     const requestOptions: any = {
-      method: options['method'] || 'GET',
-      headers: options['headers'] ? new HttpHeaders(options['headers']) : this.defaultHeaders,
+      headers: {...options['headers'], timeout: '120000'},
       params: queryParameters,
-      body: options['body'] || undefined
+      body: options['body'] || undefined,
+      observe: 'response'
     };
 
-    switch (resource) {
-      case '/autocomplete/taxon':
-        queryParameters.set(
-          'excludeNameTypes',
-          'MX.misappliedCircumscription,MX.misspelledCircumscription,MX.uncertainCircumscription'
-        );
-    }
-
-    return this.http.request(path, requestOptions).toPromise(Promise);
+    return this.http.request(
+      method, path, requestOptions
+    ).pipe(
+      map((response: any) => ({...response, json: () => response.body})),
+      catchError(err => of({...err, json: () => err.error})),
+    ).toPromise(Promise);
   }
 }
