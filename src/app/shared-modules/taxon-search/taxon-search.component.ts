7a835c85c0f1d0337a41ee0d412a26f7640ad465
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2024-07-11 21:22:53 +0300

    fix obs map taxon search

--- src/app/shared-modules/taxon-search/taxon-search.component.ts
+++ src/app/shared-modules/taxon-search/taxon-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Renderer2, Inject, PLATFORM_ID, Output, EventEmitter, OnDestroy, ViewChild, ElementRef, AfterViewInit } from "@angular/core";
+import { Component, OnInit, Renderer2, Inject, PLATFORM_ID, Output, EventEmitter, OnDestroy, ViewChild, ElementRef, AfterViewInit, ChangeDetectorRef } from "@angular/core";
 import * as $ from 'jquery';
 import { TranslateService } from "@ngx-translate/core";
 import { isPlatformBrowser } from "@angular/common";
@@ -6,53 +6,61 @@ import { TaxonService } from "app/shared/service/taxon.service";
 import { Taxonomy } from "app/shared/model";
 import { fromEvent, Subject } from "rxjs";
 import { debounceTime, takeUntil, filter, map, switchMap } from "rxjs/operators";
+import { ChangeDetectionStrategy } from "@angular/compiler";
 
 @Component({
     selector: "vrs-taxon-search",
     templateUrl: './taxon-search.component.html',
-    styleUrls: ["./taxon-search.component.scss"]
+    styleUrls: ["./taxon-search.component.scss"],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TaxonSearchComponent implements AfterViewInit, OnDestroy {
     unsubscribe$ = new Subject<void>();
 
     autocompleteItems: [];
     selected;
-    
+
     @ViewChild('autocomplete', { static: false }) autocomplete: ElementRef;
     @ViewChild('input', { static: false }) input: ElementRef;
     @Output() taxonChange = new EventEmitter();
 
-    constructor(private taxonService: TaxonService,
-                public translate: TranslateService,
-                @Inject(PLATFORM_ID) private platformId) {}
+    constructor(
+        private taxonService: TaxonService,
+        private renderer: Renderer2,
+        private cdr: ChangeDetectorRef,
+        public translate: TranslateService,
+        @Inject(PLATFORM_ID) private platformId
+    ) {}
 
     ngAfterViewInit() {
-        if (isPlatformBrowser(this.platformId)) {
-            fromEvent(this.input.nativeElement, 'input').pipe(
-                takeUntil(this.unsubscribe$),
-                debounceTime(500)
-            ).pipe(
-                filter((event: any) => {
-                    if (event.target.value.length < 3) {
-                        this.autocompleteItems = undefined;
-                        return false;
-                    } else {
-                        return true
-                    }
-                }),
-                map(event => event.target.value),
-                switchMap(val => this.taxonService.getAutocomplete(val))
-            ).subscribe(r => {
-                this.autocompleteItems = r;
-            })
-            fromEvent(document, 'click').pipe(
-                takeUntil(this.unsubscribe$)
-            ).subscribe(event => {
-                if (!this.autocomplete.nativeElement.contains(event.target)) {
+        fromEvent(this.input.nativeElement, 'input').pipe(
+            takeUntil(this.unsubscribe$),
+            debounceTime(500)
+        ).pipe(
+            filter((event: any) => {
+                if (event.target.value.length < 3) {
                     this.autocompleteItems = undefined;
+                    this.cdr.markForCheck();
+                    return false;
+                } else {
+                    return true
                 }
-            })
-        }
+            }),
+            map(event => event.target.value),
+            switchMap(val => this.taxonService.getAutocomplete(val))
+        ).subscribe(r => {
+            this.autocompleteItems = r;
+            this.cdr.markForCheck();
+        });
+
+        fromEvent(document, 'click').pipe(
+            takeUntil(this.unsubscribe$)
+        ).subscribe(event => {
+            if (!this.autocomplete.nativeElement.contains(event.target)) {
+                this.autocompleteItems = undefined;
+                this.cdr.markForCheck();
+            }
+        })
     }
 
     onSelect(item) {
@@ -94,6 +102,7 @@ export class TaxonSearchComponent implements AfterViewInit, OnDestroy {
             this.input.nativeElement.disabled = false;
             this.input.nativeElement.value = '';
         }
+        this.cdr.markForCheck();
     }
 
     ngOnDestroy() {
