d82c5982e8b3deaaea1ce2ba88e19969fab63d99
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2026-08-14 09:49:05 +0300

    update sitemap

--- src/generate_sitemap.ts
+++ src/generate_sitemap.ts
@@ -1,37 +1,43 @@
-const http = require('http');
+import type { PagedResult, Taxonomy } from "app/shared/model";
+
 const fs = require('fs');
+const path = require('path');
 
-var options = {
-    host: 'vieraslajit-dev.laji.fi'
-    ,port: 80
-    ,path: '/api/taxa/MX.37600/species?page=1&pageSize=400&invasiveSpeciesFilter=true&lang=fi&selectedFields=id'
-    ,method: 'GET'
-    ,headers: { 'Content-Type': 'application/json' }
-  };
+const apiUrl = 'https://vieraslajit.fi/api';
+const rootTaxonId = 'MX.37600';
+const pageSize = 10000;
+const sitemapPath = path.join(__dirname, 'assets', 'sitemap');
 
-http.get(
-    options,
-    (res) => {
-        let data = [];
-        res.on('data', (d) => {
-            data.push(d);
-        });
-        res.on('end', () => {
-            // @ts-ignore
-            const parsed = JSON.parse(data);
-            const species: any[] = parsed.results;
-            const ids: string[] = species.map(s => s.id);
-            const urlStart = 'https://vieraslajit-dev.laji.fi/taxon/'
-            const speciesUrls = ids.map(id => urlStart + id);
-            let sitemap = '';
-            for (let url of speciesUrls) {
-                sitemap = sitemap += url + '\n';
-            }
-            
-            fs.writeFile('./assets/sitemap', sitemap, (err) => {  
-                if (err) throw err;
-                console.log('Sitemap saved!');
-            });
-        });
-    }
-)
+async function getTaxonPage(page: number): Promise<PagedResult<Taxonomy>> {
+  const url = `${apiUrl}/taxa/${rootTaxonId}/species?page=${page}`
+    + `&pageSize=${pageSize}&invasiveSpeciesFilter=true&lang=fi&selectedFields=id`;
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`API request failed with status ${response.status}`);
+  }
+
+  return response.json();
+}
+
+async function generateSitemap(): Promise<void> {
+  const firstPage = await getTaxonPage(1);
+  const taxa = firstPage.results.slice();
+
+  for (let page = 2; page <= firstPage.lastPage; page++) {
+    const taxonPage = await getTaxonPage(page);
+    taxa.push(...taxonPage.results);
+  }
+
+  const sitemap = taxa
+    .map(taxon => `https://vieraslajit.fi/lajit/${taxon.id}`)
+    .join('\n') + '\n';
+
+  await fs.promises.writeFile(sitemapPath, sitemap, 'utf8');
+  console.log(`Sitemap saved to ${sitemapPath} (${taxa.length} taxa)`);
+}
+
+generateSitemap().catch(error => {
+  console.error('Failed to generate sitemap:', error);
+  process.exitCode = 1;
+});
